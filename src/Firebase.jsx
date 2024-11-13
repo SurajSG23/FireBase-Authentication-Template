9266b82c09d7d6bceca1146447d73d73c8d5c170
Suraj S G Dhanva <164147518+SurajSG23@users.noreply.github.com>
2024-11-13 14:45:07 +0530

    Update Firebase.jsx

--- src/Firebase.jsx
+++ src/Firebase.jsx
@@ -7,14 +7,15 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAysqkod1o7C9kE6Q2R3WnQGMztqTTAZVA",
-  authDomain: "practice-set-d077b.firebaseapp.com",
-  projectId: "practice-set-d077b",
-  storageBucket: "practice-set-d077b.firebasestorage.app",
-  messagingSenderId: "811764569196",
-  appId: "1:811764569196:web:fdbf15670375d3445eb449",
-  measurementId: "G-K6CB1F1X75",
-  databaseURL:"https://practice-set-d077b-default-rtdb.firebaseio.com/",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+  //Add the below file:
+  databaseURL:"",
 };
 
 // Initialize Firebase
